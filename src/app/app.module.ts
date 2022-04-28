e8b14b144ce098c555c22eb89b7e3fe1779e8870
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-04-27 19:38:50 -0500

    Class 45: Create component automatically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter.component';
 import { SuperheroComponent } from './superheros/superhero/superhero.component';
+import { ListComponent } from './superheros/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, SuperheroComponent],
+  declarations: [
+    AppComponent,
+    CounterComponent,
+    SuperheroComponent,
+    ListComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
