e47013284a10ecac3eba0aaf43bedc9905ea11dd
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-04-27 19:20:17 -0500

    Class 44: One way data binding

--- src/app/superheros/superhero/superhero.component.ts
+++ src/app/superheros/superhero/superhero.component.ts
@@ -7,10 +7,16 @@ import { Component } from '@angular/core';
 export class SuperheroComponent {
   public name: string = 'Ironman';
   public age: number = 40;
+  get getNameCapitalized(): string {
+    return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+  }
   getName(): string {
     return `${this.name} - ${this.age}`;
   }
-  get getNameCapitalized(): string {
-    return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+  changeName(): void {
+    this.name = 'Spiderman';
+  }
+  changeAge(): void {
+    this.age = 30;
   }
 }
