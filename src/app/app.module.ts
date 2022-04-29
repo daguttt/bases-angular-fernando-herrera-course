a58a1bc183f3b8dacf8bddaa524419c16833bbe1
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-04-28 21:24:14 -0500

    Class 50: Modules part two
    - Create a module for CounterComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter.component';
+import { CounterModule } from './counter/counter.module';
 import { SuperheroesModule } from './superheroes/superheroes.module';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
-  imports: [BrowserModule, SuperheroesModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SuperheroesModule, CounterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
