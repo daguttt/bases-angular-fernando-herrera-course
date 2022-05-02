775eaa0694af1be80f884a6de10e3d33b7b88659
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-02 18:55:20 -0500

    Class 61. Create children components

---
+++ src/app/dbz/character-list/character-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-list',
+  templateUrl: './character-list.component.html',
+})
+export class CharacterListComponent {
+  public characterList: any[] = [];
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
+import { CharacterListComponent } from './character-list/character-list.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, CharacterListComponent],
   exports: [MainPageComponent],
   imports: [CommonModule, FormsModule],
 })
