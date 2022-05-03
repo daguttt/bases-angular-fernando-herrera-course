6c4875a799fefa9199364548bccf4885a82322e2
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-03 14:06:32 -0500

    Prepare app to its first build

--- src/app/dbz/add-character/add-character.component.ts
+++ src/app/dbz/add-character/add-character.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Character } from '../interfaces/dbz.interface';
 import { DbzService } from '../services/dbz.service';
 
